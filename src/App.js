639c3951dd0243a31cfdb69f14180b34ef1b78f0
Robert B Garcia <robgarcia552@gmail.com>
2019-09-08 16:37:10 -0400

    style app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
+
+
+const center = {
+  width: '400px',
+  margin: '15% auto',
+  textAlign: 'center'
+}
+
+const buttons = {
+  padding: '10px',
+  margin: '10px',
+  background: 'lightblue',
+  borderRadius: '20px'
+}
+
 const App = () => {
   const SNIPPETS = [
     'Bears, beets, battlestar galactica',
@@ -35,22 +50,24 @@ const App = () => {
   });
   
   return (
-    <div>
-      <h1>Elevate your typing skills!</h1>
-      <hr />
-      <h3>Snippet</h3>
-      <p>{snippet}</p>
-      <h2>{gameState.victory ? `Done! 🎉 Time: ${gameState.endTime}ms` : null}</h2>
-      <input value={text} onChange={updateText}/>
-      <hr />
-      {
-        SNIPPETS.map((SNIPPET, index) => (
-          <button onClick={chooseSnippet(index)} key={index}>
-            {SNIPPET.substring(0, 10)}...
-          </button>
-        ))
-      }
-    </div>
+   
+      <div style={center}>
+        <h1>Elevate your typing skills!</h1>
+        <hr />
+        <h3>Snippet</h3>
+        <p>{snippet}</p>
+        <h2>{gameState.victory ? `Done! 🎉 Time: ${gameState.endTime}ms` : null}</h2>
+        <input value={text} onChange={updateText}/>
+        <hr />
+        {
+          SNIPPETS.map((SNIPPET, index) => (
+            <button onClick={chooseSnippet(index)} key={index} style={buttons}>
+              Challenge #{index+1}
+            </button>
+          ))
+        }
+      </div>
+  
   );
 }
 
