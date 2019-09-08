32eee7ca0ddd450ea32fcee221adc097f3853bbd
Robert B Garcia <robgarcia552@gmail.com>
2019-09-08 15:58:43 -0400

    add snippets, game logic, functions to update the chosen snippet and victory

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import React, { useState } from 'react';
 
 const App = () => {
+  const SNIPPETS = [
+    'Bears, beets, battlestar galactica',
+    "What's Forrest Gump's password? 1Forrest1",
+    'Where do programmers like to hangout? The Foo Bar'
+  ];
+
+  const INITIAL_GAME_STATE = { victory: false, startTime: null, endTime: null };
+  const [snippet, setSnippet] = useState('');
   const [text, setUserText] = useState('');
+  const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
 
   const updateText = event => {
     setUserText(event.target.value);
-    console.log('Current text', text);
+
+    if (event.target.value === snippet) {
+      setGameState({
+        ...gameState,
+        victory: true,
+        endTime: new Date().getTime() - gameState.startTime
+      });
+    }
+  }
+
+  const chooseSnippet = snippetIndex => () => {
+    console.log('setSnippet', snippetIndex);
+    setSnippet(SNIPPETS[snippetIndex]);
+    setGameState({ ...gameState, startTime: new Date().getTime() });
   }
   
   return (
     <div>
       <h1>Elevate your typing skills!</h1>
+      <hr />
+      <h3>Snippet</h3>
+      <p>{snippet}</p>
+      <h2>{gameState.victory ? `Done! 🎉 Time: ${gameState.endTime}ms` : null}</h2>
       <input value={text} onChange={updateText}/>
+      <hr />
+      {
+        SNIPPETS.map((SNIPPET, index) => (
+          <button onClick={chooseSnippet(index)} key={index}>
+            {SNIPPET.substring(0, 10)}...
+          </button>
+        ))
+      }
     </div>
   );
 }
